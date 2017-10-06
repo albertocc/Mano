72e66e20e9f61d3f37fbdeaaf14267e6841da94b
Alberto Cabeza Cardiel/IoT Solutions (CS)/./Samsung Electronics <a.cabeza@samsung.com>
2017-10-06 17:29:16 +0200

    Initial commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.FlatAppearance.BorderSize = 0;
+            this.btnLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeft.Location = new System.Drawing.Point(12, 12);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(143, 133);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnLeft_MouseDown);
+            // 
+            // btnRight
+            // 
+            this.btnRight.FlatAppearance.BorderSize = 0;
+            this.btnRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRight.Location = new System.Drawing.Point(161, 12);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(143, 133);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnRight_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 157);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -6,15 +6,40 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        private static extern int SwapMouseButton(int bSwap);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRight_MouseDown(object sender, EventArgs e)
+        {
+            // Cambia
+            SwapMouseButton(0);
+            // Lo guarda en el registro
+            Registry.CurrentUser.CreateSubKey(@"Control Panel\Mouse").SetValue("SwapMouseButtons", 0);
+            // Muestra el actual en el registro (no cambia hasta cerrar sesion)
+            MessageBox.Show(SystemInformation.MouseButtonsSwapped ? "1" : "0", "Right", MessageBoxButtons.OK);
+            Application.Exit();
+        }
+
+        private void btnLeft_MouseDown(object sender, EventArgs e)
+        {
+            SwapMouseButton(1);
+            // 1 es Left. 0 es Right.
+            Registry.CurrentUser.CreateSubKey(@"Control Panel\Mouse").SetValue("SwapMouseButtons", 1);
+            MessageBox.Show(SystemInformation.MouseButtonsSwapped ? "1" : "0", "Left", MessageBoxButtons.OK);
+            Application.Exit();
         }
     }
 }
